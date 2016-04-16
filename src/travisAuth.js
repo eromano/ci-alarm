c45be7fefa9af3c19a43bb2da481692f328f5b88
Eugenio Romano <eugenio.romano@alfresco.com>
2016-04-16 15:54:08 +0200

    :nail_care: Auth test first part

--- src/travisAuth.js
+++ src/travisAuth.js
@@ -3,17 +3,27 @@ var assert = require('assert');
 
 class travisAuth {
 
-    constructor(travis, githubToken) {
+    /**
+     * constructor travisAuth class necessary to login in travis with the githubToken
+     *
+     * @param  {Object} travisService object
+     * @param  {String} githubToken github Personal access token get from the config.json see the readme to understand how to get it
+     */
+    constructor(travisService, githubToken) {
+        assert(travisService, 'Travis service is necessary, instantiate a travis service');
         assert(githubToken, 'githubtoken in config.json is necessary');
 
         this.githubToken = githubToken;
-        this.travis = travis;
+        this.travis = travisService;
     }
 
+    /**
+     * login Travis Service through GitHub Token
+     */
     login() {
         return new Promise((resolve, reject) => {
-            this.authenticateGitHub().then((res) => {
-                this.authenticateTravis(res).then(() => {
+            this._authenticateGitHub().then((res) => {
+                this._authenticateTravis(res).then(() => {
                     resolve();
                 }, (err) => {
                     reject(err);
@@ -24,7 +34,7 @@ class travisAuth {
         });
     }
 
-    authenticateGitHub() {
+    _authenticateGitHub() {
         return new Promise((resolve, reject) => {
             this.travis.auth.github.post({
                 github_token: this.githubToken
@@ -40,7 +50,7 @@ class travisAuth {
         });
     }
 
-    authenticateTravis(res) {
+    _authenticateTravis(res) {
         return new Promise((resolve, reject) => {
             this.travis.authenticate({
                 access_token: res.access_token
---
+++ test/travisAuth.spec.js
@@ -0,0 +1,65 @@
+/*jshint expr:true */
+/*global describe, it */
+'use strict';
+
+var TravisAuth = require('../src/travisAuth');
+var TravisService = require('../src/travisService');
+
+var expect = require('chai').expect;
+
+describe('Travis Auth', function () {
+
+    describe('Init', function () {
+
+        it('Should throw an error if no Travis service is passed', function () {
+            var errorThrown = false;
+            try {
+                new TravisAuth(null, 'fake-token');
+            } catch (error) {
+                errorThrown = true;
+            }
+
+            expect(errorThrown).equals(true);
+        });
+
+        it('Should throw an error if no GitHub token is passed', function () {
+            var errorThrown = false;
+            try {
+                new TravisAuth(new TravisService('fake-token'), null);
+            } catch (error) {
+                errorThrown = true;
+            }
+
+            expect(errorThrown).equals(true);
+        });
+
+        it('Should init be ok if Travis service and github token is passed', function () {
+            var errorThrown = false;
+
+            try {
+                new TravisAuth(new TravisService('fake-token'), 'fake-token');
+            } catch (error) {
+                errorThrown = true;
+            }
+
+            expect(errorThrown).equals(false);
+        });
+
+    });
+
+    //describe('login Github', function () {
+    //
+    //    it('Should throw an error if the Github token is not valid', function () {
+    //        var errorThrown = false;
+    //        try {
+    //            var travisAuth  = new TravisAuth('fake-token');
+    //            travisAuth.authenticateGitHub();
+    //        } catch (error) {
+    //            errorThrown = true;
+    //        }
+    //
+    //        expect(errorThrown).equals(true);
+    //    });
+    //
+    //});
+});
