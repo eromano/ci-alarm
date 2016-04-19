79894736e49965f354750b45526c1f25bba754a4
Eugenio Romano <eugenio.romano@alfresco.com>
2016-04-20 00:29:07 +0100

    unused semicolon

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ var nconf = require('nconf');
 nconf.add('config', {type: 'file', file: './config.json'});
 
 try {
-    var tokenSlack = process.env.TOKEN_SLACK || nconf.get('tokenslack');;
+    var tokenSlack = process.env.TOKEN_SLACK || nconf.get('tokenslack');
     var githubToken = process.env.TOKEN_GITHUB || nconf.get('githubtoken');
 
     new CiAlarmBot(tokenSlack, githubToken);
